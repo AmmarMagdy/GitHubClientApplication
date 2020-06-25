d2e26f5847bbdd60e5589bb37b8ae49694d65bbd
AmmarMagdy <ammar.m.badawi@gmail.com>
2020-06-25 19:17:50 +0200

    Add Repository Model

---
+++ GitHubClientApplication/Model/Repository.swift
@@ -0,0 +1,19 @@
+//
+//  Repository.swift
+//  GitHubClientApplication
+//
+//  Created by Ammar.M on 6/25/20.
+//  Copyright © 2020 Ammar.M. All rights reserved.
+//
+
+import Foundation
+
+struct Repository: Codable {
+    
+    let fullName : String?
+    let id : Int?
+    let name : String?
+    let nodeId : String?
+    let privateField : Bool?
+    
+}

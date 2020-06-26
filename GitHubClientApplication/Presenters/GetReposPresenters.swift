6444d4a7999a9134951f75adf1f4fdb770de9ae7
AmmarMagdy <ammar.m.badawi@gmail.com>
2020-06-26 20:13:52 +0200

    add presenter file

--- GitHubClientApplication/Presenters/GetReposPresenters.swift
+++ GitHubClientApplication/Presenters/GetReposPresenters.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+protocol GetRepoView: AnyObject {
+    func showRepos(_ repos: [Repository])
+}
+
+protocol GetRepoPresenter {
+    func fetchReposFromAPI()
+}
+
+class GetReposImpl: GetRepoPresenter {
+    
+    weak var view: GetRepoView?
+    var repos: [Repository]!
+    
+    init(view: GetRepoView) {
+        self.view = view
+    }
+    
+    func fetchReposFromAPI() {
+        Request.requestAPI(router: Router.getAllRepositories) { [unowned self](response) in
+            switch response {
+            case .success(let result):
+                do {
+                    self.repos = try JSONDecoder().decode([Repository].self, from: result)
+                    self.view?.showRepos(self.repos)
+                } catch let error {
+                    print(error)
+                }
+            case .failure(let error):
+                Alert.shared.showAlert(title: "Error", message: error.localizedDescription)
+            }
+        }
+    }
+    
+}
+
+
